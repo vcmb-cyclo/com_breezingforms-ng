f3427de8b0e2b6bb87042c7443b474614a08da80
Xavier DANO <xavier.dano@heolween.com>
2026-07-20 17:57:21 +0200

    Use injected Joomla model context services

--- administrator/components/com_breezingformsng/src/Model/ImportModel.php
+++ administrator/components/com_breezingformsng/src/Model/ImportModel.php
@@ -141,7 +141,7 @@ class ImportModel extends BaseDatabaseModel
         }
 
         $now = (new \Joomla\CMS\Date\Date())->toSql();
-        $userName = (string) (Factory::getApplication()->getIdentity()?->username ?? '');
+        $userName = (string) ($this->getCurrentUser()?->username ?? '');
 
         if ($existing !== null) {
             $item['modified'] = $now;

cebc44c5b0995c886e7987b068140acc503bea6b
Xavier DANO <xavier.dano@heolween.com>
2026-09-01 11:39:59 +0200

    Normalize ContentBuilder metadata loader

--- components/com_breezingformsng/src/Service/Rendering/ContentBuilderFormMetadataLoader.php
+++ components/com_breezingformsng/src/Service/Rendering/ContentBuilderFormMetadataLoader.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * BreezingForms NG - A Joomla Forms Application
+ *
+ * @package BreezingFormsNG
+ * @copyright Copyright (C) 2026 by XDA+GIL
+ * @license GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ **/
+
 declare(strict_types=1);
 
 namespace Vcmb\Component\BreezingformsNG\Site\Service\Rendering;
